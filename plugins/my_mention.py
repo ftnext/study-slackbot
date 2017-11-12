d7e07edcd72e1def3c18a5eabfde8c342a7b6423
nikkie <takuyafjp@gmail.com>
2017-11-12 18:59:10 +0900

    案内メッセージへのリンクの追加を実装
    * 改行を含んでリンクを渡した場合slackbotが想定通り動作しなかったので、半角スペースを用いるように変更（plugins/explain.txt）
    * メンション付き投稿で受け取ったリンクを加工し、案内リストの変数に追加するadd_link_listメソッドを実装
    * 変更方法の案内をBotへのメンションでも表示できるようにhow_to_change_guideメソッドを修正（リンク追加後の確認で使うことを想定）

--- plugins/my_mention.py
+++ plugins/my_mention.py
@@ -21,6 +21,7 @@ def guide_to_new_menber(message):
 
 @listen_to('案内.*変更')
 @listen_to('案内.*変え')
+@respond_to('いま|今|現在.*案内')
 def how_to_change_guide(message):
     # チャネルに投稿された 案内の変更に関する文言 に反応し、
     # 変更方法の案内をスレッドで返す
@@ -32,6 +33,33 @@ def how_to_change_guide(message):
     for link in link_list:
         current_links += '{0} : {1}\n'.format(index, link)
         index += 1
-    # 説明文ファイル中のリンクリスト部分に現在のリンクリストを入れる
+    # 説明文ファイル中に現在の案内リストを入れる
     explain = explain.format(current_links)
     message.reply(explain, in_thread=True)
+
+@respond_to('案内.*追加.*')
+def add_link_list(message):
+    global link_list
+    if len(link_list) == 3:
+        reply_msg = '案内リストが3件あり、これ以上追加できません'
+    else: # len(link_list) <= 2
+        text = message.body['text']
+        # リンクの前にスペースを含ませるよう案内しているので、スペースで分割する
+        splited_text = text.split(' ')
+        if len(splited_text) <= 1:
+            reply_msg = 'リンクが含まれていないようです。案内リストに追加できませんでした'
+        elif len(splited_text) >= 3:
+            reply_msg = '複数のリンクが渡されたようです。申し訳ありませんが、一度に一つしか追加できません'
+        else: # len(splited_text) == 2
+            add_link = splited_text[1]
+            # add_linkは<url>という形式になっている
+            # リンクがteam内の投稿かどうか確認する
+            assumed_pattern = '<https://nikkie-project.slack.com/archives/'
+            if not assumed_pattern in add_link:
+                reply_msg = 'このteam内のリンクではないようです。このteamの投稿のリンクにしてください'
+            else:
+                #<url>という形式から先頭の<と末尾の>を除く
+                add_link = add_link[1:len(add_link)-1]
+                link_list.append(add_link)
+                reply_msg = '案内リストに追加しました'
+    message.reply(reply_msg, in_thread=True)
