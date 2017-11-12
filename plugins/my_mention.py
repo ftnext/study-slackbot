6938d0838c691c12110df69b689165824fb65236
nikkie <takuyafjp@gmail.com>
2017-11-12 16:05:14 +0900

    案内メッセージの変更方法を通知する
    * チャネルへの投稿に反応(@listen_toとre)
    * 案内メッセージの変更方法の固定文言はplugins/explain.txtにまとめた
    * 現在の案内リストの部分だけは最新の情報をもとに組み立て、変更方法の文言に当てはめる

--- plugins/my_mention.py
+++ plugins/my_mention.py
@@ -1,5 +1,6 @@
 from slackbot.bot import respond_to     # @botname: で反応するデコーダ
 from slackbot.bot import listen_to      # チャネル内発言で反応するデコーダ
+import re
 
 link_list = ['https://nikkie-project.slack.com/archives/C7ZPKC2RM/p1510391262000028']
 
@@ -17,3 +18,20 @@ def guide_to_new_menber(message):
     guide = 'このチャネルの使い方はこちらをご覧ください\n'
     guide += '\n'.join(link_list)
     message.reply(guide)
+
+@listen_to('案内.*変更')
+@listen_to('案内.*変え')
+def how_to_change_guide(message):
+    # チャネルに投稿された 案内の変更に関する文言 に反応し、
+    # 変更方法の案内をスレッドで返す
+    explain_file = open('plugins/explain.txt', 'r')
+    explain = explain_file.read()
+    index = 0
+    current_links = ''
+    global link_list
+    for link in link_list:
+        current_links += '{0} : {1}\n'.format(index, link)
+        index += 1
+    # 説明文ファイル中のリンクリスト部分に現在のリンクリストを入れる
+    explain = explain.format(current_links)
+    message.reply(explain, in_thread=True)
