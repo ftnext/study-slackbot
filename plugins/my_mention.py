e5f0347be43fe3fa2f8444fe3a78580581fc7313
nikkie <takuyafjp@gmail.com>
2017-11-12 14:54:50 +0900

    特定の言葉を含むチャネルへの投稿に反応する
    * チャネルの案内を表示する
    
    TODO：Botと会話して案内をカスタマイズする

--- plugins/my_mention.py
+++ plugins/my_mention.py
@@ -1,7 +1,19 @@
 from slackbot.bot import respond_to     # @botname: で反応するデコーダ
+from slackbot.bot import listen_to      # チャネル内発言で反応するデコーダ
+
+link_list = ['https://nikkie-project.slack.com/archives/C7ZPKC2RM/p1510391262000028']
 
 @respond_to('だんない')
 def mention_dannnai(message):
     # @Bot だんない に対して返答する
     message.reply('ありがとうございます:taco:')
     message.react('grinning')
+
+@listen_to('はじめまして')
+def guide_to_new_menber(message):
+    # チャネルに投稿された はじめまして に反応し、
+    # アーカイブのリンクを貼ってチャネルの案内をする
+    global link_list
+    guide = 'このチャネルの使い方はこちらをご覧ください\n'
+    guide += '\n'.join(link_list)
+    message.reply(guide)
